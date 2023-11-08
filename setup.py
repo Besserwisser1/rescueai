e7856826f3768cf23022bf63481419078e39dd9a
Besserwisser1 <einBesserwisser@yandex.ru>
2023-11-08 23:50:48 +0700

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='dsworks 2023',
+    author='dsworks',
+    license='MIT',
+)
